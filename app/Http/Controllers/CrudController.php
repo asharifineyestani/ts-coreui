acbe9e0ec941ff41d07c1c6555ba28167bee2e27
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-08-26 20:47:25 +0430

    added storeMedia

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -150,4 +150,26 @@ class CrudController extends Controller
 
     }
 
+
+    public function storeMedia(Request $request)
+    {
+        $path = storage_path('tmp');
+
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        $file = $request->file('file');
+
+        $name = uniqid() . '_' . trim($file->getClientOriginalName());
+
+        $file->move($path, $name);
+
+        return response()->json([
+            'name'          => $name,
+            'original_name' => $file->getClientOriginalName(),
+        ]);
+
+    }
+
 }
