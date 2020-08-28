fe4bf9bafdee0a383d3811591dae6e9f5a2b875e
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-08-28 20:19:57 +0430

    handle video on mediable

--- resources/views/afra/fields/mediable.blade.php
+++ resources/views/afra/fields/mediable.blade.php
@@ -1,17 +1,17 @@
 @php
     $result = [];
       if($crud->row) {
-         $medias = $crud->row->getMedia('*');
+         $medias = $crud->row->media;
           foreach ($medias as $media)
               $result[] = ([
                   "name" => $media->name,
                   "size" => $media->size,
                   "url" => $media->getUrl(),
+                  "type" => $media->type,
               ]);
   }
 
 @endphp
-
 
 
 <div class="form-group">
@@ -23,6 +23,7 @@
 @push('fields_scripts')
 
     <script src="/js/dropzone.min.js"></script>
+
 
     <script>
         let uploadedMediableMap = {}
@@ -39,18 +40,9 @@
             },
             removedfile: function (file) {
 
-
-                $.ajax('/crud/deleteMedia/'+file.name, {
+                $.ajax('/crud/deleteMedia/' + file.name, {
                     type: 'GET',
-                    // data: {name: file.name},
-                    // success: function (data, status, xhr) {
-                    //     console.log('status: ' + status + ', data: ' + data);
-                    // },
-                    // error: function (jqXhr, textStatus, errorMessage) {
-                    //     console.log('Error' + errorMessage);
-                    // }
                 });
-
 
                 console.log(file)
                 file.previewElement.remove()
@@ -65,14 +57,59 @@
             init: function () {
                 let mockFile = '';
 
-                @foreach($result as $media)
 
+                @foreach($result as $media)
                     mockFile = {!! json_encode($media) !!};
-                this.emit("addedfile", mockFile);
-                this.emit("thumbnail", mockFile, mockFile.url);
-                this.emit("complete", mockFile);
+
+                if (mockFile.type == "video") {
+                    video(mockFile)
+                } else {
+                    this.emit("addedfile", mockFile);
+                    this.emit("thumbnail", mockFile, mockFile.url);
+                    this.emit("complete", mockFile);
+                }
+
+
                 @endforeach
             }
+        }
+
+
+        function video(file) {
+            var src = file.url; ///video url not youtube or vimeo,just video on server
+            var video = document.createElement('video');
+            video.src = src;
+
+            video.width = 120;
+            video.height = 106;
+
+            var canvas = document.createElement('canvas');
+            canvas.width = 360;
+            canvas.height = 240;
+            var context = canvas.getContext('2d');
+
+            video.addEventListener('loadeddata', function () {
+                context.drawImage(video, 0, 0, canvas.width, canvas.height);
+                var dataURI = canvas.toDataURL('image/jpeg');
+                html += '<figure>';
+                html += '<img src="' + dataURI + '' + '" alt="' + item.description + '" />';
+                html += '<figurecaption>' + item.description + '</figurecaption>'
+                html += '</figure>';
+            });
+
+            let icon = document.createElement('span');
+            icon.setAttribute('class', 'video-icon');
+            icon.innerHTML = "video"
+            let div = document.createElement('div');
+            div.setAttribute('class', 'wrap-video');
+            div.appendChild(video);
+            div.appendChild(icon);
+
+
+            console.log(div)
+            console.log(div)
+
+            $('#mediable-dropzone').append(div);
         }
     </script>
 
@@ -82,6 +119,32 @@
 
 @push('fields_css')
     <link href="/js/dropzone.min.css" rel="stylesheet">
+
+
+    <style>
+
+        .wrap-video {
+            display: inline-block;
+            position: relative;
+            padding: 0px !important;
+            margin: 0px !important;
+        }
+
+
+        .video-icon {
+            background: red;
+            padding: 2px 4px;
+            border-radius: 4px;
+            text-align: center;
+            color: white;
+            background: #333;
+            opacity: .4;
+            position: absolute;
+            left: 38px;
+            top: 45px;
+            font-size: 12px;
+        }
+    </style>
 @endpush
 
 
